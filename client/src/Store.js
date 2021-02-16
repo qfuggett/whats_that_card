3ae934d8c0bf2ea4fefd51dd88cfdd26c2908192
QueenTesa Fuggett <qfuggett@umich.edu>
2021-02-16 04:40:01 -0500

    insert Provider and import RootReducer

--- client/src/Store.js
+++ client/src/Store.js
@@ -2,5 +2,8 @@ import {createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {applyMiddleware} from "redux";
 import thunk from "redux-thunk";
+import RootReducer from "./reducers/RootReducer"
 
-const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const Store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default Store;
